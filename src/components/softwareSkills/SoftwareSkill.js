131e03235bf72d3d2a4f7175dc46e09c4a4a4083
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-06 20:51:48 +0500

    feat: added softwares

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -1,21 +1,68 @@
 import React from "react";
+import {TbBrandNextjs} from "react-icons/tb";
+import {
+  SiTailwindcss,
+  SiJavascript,
+  SiFirebase,
+  SiTypescript,
+  SiSass,
+  SiHtml5,
+  SiCss3
+} from "react-icons/si";
+import {GrReactjs} from "react-icons/gr";
 import "./SoftwareSkill.scss";
 import {skillsSection} from "../../portfolio";
 
 export default function SoftwareSkill() {
+  const socialLinks = [
+    {
+      name: "NextJs",
+      icon: TbBrandNextjs
+    },
+    {
+      name: "ReactJs",
+      icon: GrReactjs
+    },
+    {
+      name: "Javascript",
+      icon: SiJavascript
+    },
+    {
+      name: "Typescript",
+      icon: SiTypescript
+    },
+    {
+      name: "Firebase",
+      icon: SiFirebase
+    },
+    {
+      name: "TailwindCss",
+      icon: SiTailwindcss
+    },
+    {
+      name: "Html",
+      icon: SiHtml5
+    },
+    {
+      name: "Sass",
+      icon: SiSass
+    },
+    {
+      name: "Css",
+      icon: SiCss3
+    }
+  ];
+
   return (
     <div>
       <div className="software-skills-main-div">
         <ul className="dev-icons">
-          {skillsSection.softwareSkills.map((skills, i) => {
+          {socialLinks.map((skills, i) => {
+            const Icon = skills.icon;
             return (
-              <li
-                key={i}
-                className="software-skill-inline"
-                name={skills.skillName}
-              >
-                <i className={skills.fontAwesomeClassname}></i>
-                <p>{skills.skillName}</p>
+              <li key={i} className="software-skill-inline" name={skills.name}>
+                <Icon style={{color: "#868e96"}} />
+                <p>{skills.name}</p>
               </li>
             );
           })}
